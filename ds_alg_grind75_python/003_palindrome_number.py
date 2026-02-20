2c1170146f59e542d130f6de6b3acabdfbef891b
Anett Keszler <anettkeszler@Anetts-MacBook-Pro.local>
2026-02-20 14:01:29 +0100

    refactor valid anagram and palindrom number

--- ds_alg_grind75_python/003_palindrome_number.py
+++ ds_alg_grind75_python/003_palindrome_number.py
@@ -17,6 +17,9 @@ Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 """
 
+# Source: https://www.youtube.com/watch?v=8u5dz7-96dk
+
+# Solution 1: convert to string
 def isPalindrome(num: int):
     str_num = str(num)
 
@@ -35,8 +38,7 @@ print(isPalindrome(-121))
 print(isPalindrome(10))
 
 
-# OTHER SOLUTIONS:
-
+# Solution 2: reverse with sliceing function
 def isPalindrome_2(num:int):
     if num < 0:
         return False
@@ -49,9 +51,25 @@ print(isPalindrome_2(10))
 
 # Without converting number to string:
 def isPalindrome_3(num):
-    return True
+
+    if num < 0 or (num % 10 == 0 and num  != 0): # if number less than 0 or number ends with 0 (there is no remainder when we divide by 10) return False
+        return False
+    
+    reversed_num = 0 
+    while num > reversed_num: 
+        reversed_num = (reversed_num * 10) + (num % 10) # remainder of num divided by 10
+        num = num // 10 # drop the last digit
+    return num == reversed_num or num == reversed_num//10
+        
+        
+# Explanation: x = 1221 - 12 21- mirrored 
+        # we remove the last digit from input number and add it to the reversed_num 
+            # reversed_num = 1
+            # reversed_num = 12 
+        # we repeat it until the half of the number length (if even)
 
 
-print(isPalindrome_3(121))
+print(isPalindrome_3(121)) 
 print(isPalindrome_3(-121))
 print(isPalindrome_3(10))
+ 
--- ds_alg_grind75_python/004_valid_anagram.py
+++ ds_alg_grind75_python/004_valid_anagram.py
@@ -9,11 +9,42 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 """
+# Source of solutions: https://www.youtube.com/watch?v=9UtInBqnCgA&list=PLot-Xpze53lfQmTEztbgdp8ALEoydvnRQ&index=1
 
+# Solution 1
 def is_anagram(s: str, t: str):
-    if sorted(s) == sorted(t):
-        return True
-    return False
+    return sorted(s) == sorted(t)
 
 print(is_anagram("anagram", "nagaram")) # True
 print(is_anagram("rat", "car")) # False
+
+# Solution 2
+# HashMap - count the occurances of each characters in both strings
+# 1. build the 2 HashMaps: 
+#   1.) a: 3, n: 1, g: 1, r: 1, m: 1
+#   2.) a: 3, n: 1, g: 1, r: 1, m: 1
+# 2. go through the keys and compare the counts for each characters 
+
+# time and memory complexity: O(n) --> O(s+t)
+
+def is_anagram_hashmap(s: str, t: str):
+    if len(s) != len(t):
+        return False
+    
+    count_s, count_t = {}, {}
+    for i in range(len(s)):
+        count_s[s[i]] = 1 + count_s.get(s[i], 0) 
+        count_t[t[i]] = 1 + count_t.get(t[i], 0)
+    for c in count_s:
+        if count_s[c] != count_t.get(c, 0):
+            return False
+    return True
+
+print(is_anagram_hashmap("anagram", "nagaram"))
+print(is_anagram_hashmap("rat", "car"))
+
+
+
+
+
+
